7b4cde1727c4d9423aefe7d15ba8a604946feaf1
Mladen90 <mladen.markovic@hotmail.ch>
2015-03-17 01:55:52 +0100

    Klasa sa rad sa misom
    Prazna klasa

---
+++ mouse.h
@@ -0,0 +1,34 @@
+/*
+    Crashing Trashing Bots is a 3D game in freeglut
+    Copyright (C) 2015 Mladen Markovic,Tamara Plazinic,Nikola Banjac
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef MOUSE_H
+#define MOUSE_H
+
+
+#include "glut.h"
+
+
+class Mouse
+{
+public:
+    void mouse(int button,int state,int x,int y);
+};
+
+
+#endif // MOUSE_H
